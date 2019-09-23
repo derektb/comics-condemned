b9f5d3f30f36b544f1b954d418471632105f944f
Derek Timm-Brock <derektimmbrock@gmail.com>
2019-09-23 15:59:07 -0700

    corrects API bug; adds all required in-panel art to 1B and 1A and adds temp timing to 1B.  (1B will probably need some more assets for the background and sealayers)

--- src/scripts/0_setup-jinx.js
+++ src/scripts/0_setup-jinx.js
@@ -13,7 +13,8 @@ jinx.setDefaults({
 const defaultTiming = 200;
 
 window.story.state.time = {
-	SHORT:  defaultTiming,
+	XSHORT: defaultTiming*2**-1,
+	SHORT:  defaultTiming*2**0,
 	MEDIUM: defaultTiming*2**1,
 	LONG:   defaultTiming*2**2,
 	XLONG:  defaultTiming*2**3,
